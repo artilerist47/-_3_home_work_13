13817bc65ff8abb718b425acf917933a330267fe
artilerist47 <artilerist.sorok.sedmoi@yandex.ru>
2022-07-06 04:05:41 +1000

    Сделан переход к конкретному персонажу со всеми постами, и к конкретному посту. Так же работает копка назад

--- main/main_views.py
+++ main/main_views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-from utils import get_all_posts
+from utils import get_all_posts, get_post_by_id, get_posts_by_user
 
 main_blueprint = Blueprint("main_blueprint", __name__, template_folder="templates")
 search_blueprint = Blueprint("search_blueprint", __name__, template_folder="templates")
@@ -12,12 +12,24 @@ bookmarks_blueprint = Blueprint("bookmarks_blueprint", __name__, template_folder
 def main_page():
     posts = get_all_posts()
     # return render_template("index.html", posts=posts)
-    return render_template("all_posts.html", posts=posts)
+    return render_template("all_posts.html", all_posts=posts)
 
 
-@search_blueprint.get("/search/")
-def main_page():
-    return render_template("search.html")
+@search_blueprint.get("/post/<int:pk>")
+def search_post_by_id(pk):
+    post = get_post_by_id(pk)
+    return render_template("post_by_id.html", post_by_id=post)
+
+
+@search_blueprint.get("/posts/<user_name>")
+def search_post_by_user_name(user_name):
+    post = get_posts_by_user(user_name)
+    return render_template("posts_by_user_name.html", posts_by_user_name=post)
+
+
+# @search_blueprint.get("/search/")
+# def main_page():
+#     return render_template("search.html")
 
 
 @tag_blueprint.get("/tag/")
--- utils.py
+++ utils.py
@@ -6,25 +6,28 @@ def get_all_posts():
         return json.load(file)
 
 
+def get_post_by_id(pk):
+    return [post for post in get_all_posts() if pk == post["pk"]]
+    # for post in get_all_posts():
+    #     if pk == post["pk"]:
+    #         return post
+
+
 def get_posts_by_user(user_name):
-    result = [post for post in get_all_posts() if user_name in post["poster_name"].lower()]
-    return result
+    return [post for post in get_all_posts() if user_name == post["poster_name"].lower()]
+    # result = [post for post in get_all_posts() if user_name == post["poster_name"].lower()]
+    # return result
 # Функция должна вызывать ошибку ValueError если такого пользователя нет
 # и пустой список, если у пользователя нет постов.
 
 
-def get_comments_by_post_id(post_id):
-    pass
+# def get_comments_by_post_id(post_id):
+#     pass
 # Возвращает комментарии определенного поста.
 # Функция должна вызывать ошибку ValueError если такого поста нет
 # и пустой список, если у поста нет комментов.
 #
 
 
-def search_for_posts(query):
-    pass
-
-
-def get_post_by_pk(pk):
-    result = [post for post in get_all_posts() if pk in post["pk"].lower()]
-    return result
+# def search_for_posts(query):
+#     pass
